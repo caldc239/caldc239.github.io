fe693a90737fa7854baf45ed3b12d456e92f8182
caldc239 <55357449+caldc239@users.noreply.github.com>
2021-08-28 17:30:30 -0400

    + project popups + moved JS to bottom of HTML for loading purposes

--- cc_js.js
+++ cc_js.js
@@ -7,3 +7,20 @@
 		}, "slow");
 	})
 });*/
+
+$('.icon').click(function() {
+	var id = $(this).attr('id');
+	console.log(id);
+	showInfo(id);
+});
+
+$(document).mouseup(function(e) {
+	var container = $('.popup');
+	if (!container.is(e.target) && container.has(e.target).length === 0) {
+		container.hide();
+	}
+});
+
+function showInfo(id) {
+	$('#' + id + 'Popup').show();
+}
